2162a2cab7d425e9e118b413e72e2411451f9b0a
Santosh kumar <sahum3378@gmail.com>
2024-03-10 03:28:12 +0530

    add

--- swg/src/App.jsx
+++ swg/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [restList, setResList] = useState([])
+
+  useEffect(()=> {
+    fetchRes()
+  },[])
+
+  async function fetchRes(){
+    const response =await fatch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9160035&lng=77.64267889999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
+    
+    const res = await response.json();
+    console.log(res.data.cards[1])
+
+
+  }
+
+
 
   return (
     <>
-      <div className='w-40 h-40 border-5'>
-         Tailwind css
+      <div className=''>
+         
       </div>
     </>
   )
