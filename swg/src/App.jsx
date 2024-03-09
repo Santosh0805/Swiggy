5afa17a133b3fedfec98285b00bd07fc830d6875
Santosh kumar <sahum3378@gmail.com>
2024-03-10 02:55:08 +0530

    add

---
+++ swg/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-40 h-40 border-5'>
+         Tailwind css
+      </div>
+    </>
+  )
+}
+
+export default App
