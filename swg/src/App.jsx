a08a2d0c5b9ec3fbf1d839915df539347d46c646
Santosh kumar <sahum3378@gmail.com>
2024-03-10 19:10:51 +0530

    add

--- swg/src/App.jsx
+++ swg/src/App.jsx
@@ -1,29 +1,38 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Cards from './Cards';
 
 function App() {
-  const [restList, setResList] = useState([])
+  const [restList, setRestList] = useState([]);
 
-  useEffect(()=> {
-    fetchRes()
-  },[])
+  useEffect(() => {
+    fetchRes();
+  }, []);
 
-  async function fetchRes(){
-    const response =await fatch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
-  
-    const res = await response.json();
-    console.log(res.data.cards[1].card.card.gridElement.infowithstyle.restaurants);
-    setResList(res.data.cards[1].card.card.gridElement.infowithstyle.restaurants);
+  async function fetchRes() {
+    try {
+      const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9160035&lng=77.64267889999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+      if (response) {
+        const fac = await response.json();
+        console.log(fac.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setRestList(fac.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
 
+      } else {
+        console.error('Failed');
+      }
+
+    } catch (error) {
+      console.error('Error data:', error);
+    }
   }
 
   return (
     <>
-      <div className=''>
-         
-      </div>
+    {restList.length == 0 ? (<h2>Loading...</h2> ) : (<div>
+      {restList.map((ele) => (<Cards />))}
+      </div>)}
     </>
-  )
+      );
 }
 
-export default App
+export default App;
