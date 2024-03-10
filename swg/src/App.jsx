c2a20455ca9472cc6477be55a9477b5c89e14c51
Santosh kumar <sahum3378@gmail.com>
2024-03-11 02:11:01 +0530

    add

--- swg/src/App.jsx
+++ swg/src/App.jsx
@@ -13,14 +13,12 @@ function App() {
     try {
       const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9160035&lng=77.64267889999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
       if (response) {
-        const fac = await response.json();
-        console.log(fac.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-        setRestList(fac.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-
+        const res = await response.json();
+        console.log(res.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setRestList(res.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
       } else {
         console.error('Failed');
       }
-
     } catch (error) {
       console.error('Error data:', error);
     }
@@ -28,11 +26,14 @@ function App() {
 
   return (
     <>
-    {restList.length == 0 ? (<h2>Loading...</h2> ) : (<div>
-      {restList.map((ele) => (<Cards />))}
+      {restList.length == 0 ? (<h2>Loading...</h2>) : 
+      (<div>
+        {restList.map((ele) =>
+        // {console.log(ele)}
+          (<Cards key={ele.info.id} res={ele} />))}
       </div>)}
     </>
-      );
+  );
 }
 
 export default App;
